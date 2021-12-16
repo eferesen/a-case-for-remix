aae9a5065090b1c138f3ada3c7dfb4565d6aa4d0
Ahsan Imam <aimam@bills.com>
2021-12-16 16:35:19 -0700

    ✨added new cases

--- app/routes/index.tsx
+++ app/routes/index.tsx
@@ -4,12 +4,9 @@ import { useLoaderData, json, Link } from "remix";
 type IndexData = {
   resources: Array<{ name: string; url: string }>;
   demos: Array<{ name: string; to: string }>;
+  cases: Array<{ name: string; to: string }>;
 };
 
-// Loaders provide data to components and are only ever called on the server, so
-// you can connect to a database or run any server side code you want right next
-// to the component that renders it.
-// https://remix.run/api/conventions#loader
 export let loader: LoaderFunction = () => {
   let data: IndexData = {
     resources: [
@@ -39,6 +36,12 @@ export let loader: LoaderFunction = () => {
         to: "demos/params",
         name: "URL Params and Error Boundaries"
       }
+    ],
+    cases: [
+      {
+        to: "cases/actions",
+        name: "Actions"
+      },
     ]
   };
 
