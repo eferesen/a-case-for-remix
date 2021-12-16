0280586b3bf0aca46556a0383678356bfbe619d1
Ahsan Imam <aimam@bills.com>
2021-12-16 16:23:15 -0700

    create static resource

---
+++ prisma/seed.ts
@@ -0,0 +1,45 @@
+import { PrismaClient } from "@prisma/client";
+const db = new PrismaClient();
+
+async function seed() {
+  await Promise.all(
+    getLeads().map(lead => {
+      return db.joke.create({ data: lead });
+    })
+  );
+}
+
+seed();
+
+function getLeads() {
+  return [
+    {
+      firstName: 'Robert',
+      lastName: 'Johnson'
+    },
+    {
+      firstName: 'Etta',
+      lastName: 'James'
+    },
+    {
+      firstName: 'Louis',
+      lastName: 'Armstrong'
+    },
+    {
+      firstName: 'Duke',
+      lastName: 'Ellington'
+    },
+    {
+      firstName: 'Ella',
+      lastName: 'Fitzgerald'
+    },
+    {
+      firstName: 'Bo',
+      lastName: 'Diddley'
+    },
+    {
+      firstName: 'Elmore',
+      lastName: 'James'
+    }
+  ];
+}
